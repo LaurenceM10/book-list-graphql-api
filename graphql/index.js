1c95f29b02ad565e4f0ae1f00038b176c637c060
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-22 00:07:11 -0600

    Migrations with sequelize-cli

--- graphql/index.js
+++ graphql/index.js
@@ -4,12 +4,12 @@ const {
 } = require('graphql');
 
 // Require the queries to use in schema
-const queries = require('../graphql/queries/');
+const queries = require('./queries/');
 
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
-        name: 'queries',
-        field:  queries
+        name: 'Query',
+        field:  queries,
     })
 });
 
--- graphql/queries/index.js
+++ graphql/queries/index.js
@@ -1,5 +1,3 @@
 const allBooks = require('./books/allBooks');
 
-module.exports = {
-    allBooks
-};
+module.exports = allBooks;
